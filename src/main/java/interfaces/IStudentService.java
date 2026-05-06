bb88769cbd4663ed469a3602f09534b3e7bbfbd3
Raynald-Gab <raynaldgdgbarrion@gmail.com>
2026-05-06 19:04:12 +0800

    Interfaces

--- src/main/java/interfaces/IStudentService.java
+++ src/main/java/interfaces/IStudentService.java
@@ -1,4 +1,12 @@
 package interfaces;
 
+import entities.Student;
+import java.util.ArrayList;
+
 public interface IStudentService {
+    void addStudent(Student student);
+    void updateStudent(String studentId, String newName, String newEmail);
+    void removeStudent(String studentId);
+    ArrayList<Student> getAllStudents();
+    Student findStudentById(String studentId);
 }
